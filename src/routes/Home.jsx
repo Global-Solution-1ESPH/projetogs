3aab7ae1690a5063b6a809c8c8a87dc5031b6f5a
Gustavo Viega Martins Lopes <gvmlopes@gmail.com>
2024-11-16 19:16:08 -0300

    Criando o SlideShow na pagina Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,41 @@
-const Home =()=>{
-    return(
-      <>
-      <div>
-        <h1>Home</h1>
-      </div>
-      </>
-    )
-  }
-  export default Home;
+import React, { useState, useEffect } from 'react';
+
+
+const img = [
+  'src/assets/',
+  'src/assets/',
+  'src/assets/',
+];
+
+const Home = () => {
+
+//Slideshow
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextImg = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % img.length);
+  };
+
+  useEffect(() => {
+    const intervalo = setInterval(nextImg, 3000);
+    return () => clearInterval(intervalo);
+  }, []);
+
+
+
+
+  return (
+    <div style={{ position: 'relative', width: 'auto', margin: 'auto' }}>
+      <img
+        src={img[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+        style={{ width: 'auto', height: 'auto', borderRadius: '8px' }}
+      />
+    </div>
+
+
+
+  );
+};
+
+export default Home;
