b64de296d61b6282608539706b5652153471fb18
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-11-18 18:24:17 -0300

    Ajustando a altura do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 w-full h-40 flex flex-col items-center justify-center gap-10">
+    <footer className="bg-gray-800 text-white text-center py-4 w-full h-36 flex flex-col items-center justify-center gap-10">
       <h1 className="font-semibold text-xl">@2024 - Direitos Reservados</h1>
       <div className="flex w-1/2 self-center justify-center gap-8">
       <FaInstagram className="h-9 w-9 hover:text-blue-200"/>
