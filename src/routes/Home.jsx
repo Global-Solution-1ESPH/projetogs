f1d614a8b13cb6a201da62e92b50bb219698db9f
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-11-18 17:59:00 -0300

    arrumando o tamanho da paginia home e justifiicando os textos

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -20,17 +20,17 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="bg-blue-200 pt-[50px] pb-[50px] flex items-center">
-      <div className="w-1/2 p-8">
-        <h1 className="text-4xl font-bold mt-[-90px] text-black mb-6">
+    <div className="min-h-screen bg-blue-200 pt-[50px] pb-[50px] flex items-center">
+      <div className="w-1/2 p-8 flex flex-col">
+        <h1 className="text-4xl font-bold mt-[-90px] text-black mb-6 text-justify">
           Na GreenWave, transformamos o futuro com soluções inovadoras em energia sustentável, promovendo um planeta mais limpo e conectado para as próximas gerações.
         </h1>
-        <p className="text-lg text-gray-700 leading-relaxed mb-6  ">
+        <p className="text-lg text-gray-700 leading-relaxed mb-6 text-justify ">
           Somos uma empresa dedicada a fornecer soluções em energia sustentável, comprometida com a preservação do meio ambiente e o desenvolvimento de tecnologias limpas. Oferecemos energia renovável de alta qualidade, como solar e eólica, atendendo às demandas de residências, empresas e comunidades. Nossa missão é impulsionar a transição para um futuro mais verde, reduzindo a dependência de combustíveis fósseis e promovendo a conscientização sobre o impacto ambiental. Trabalhamos com inovação, responsabilidade e um profundo compromisso com um planeta mais sustentável.
         </p>
         <Link
           to="/sobre"
-          className="ml-[370px] bg-gray-800 text-white text-lg font-medium py-2 px-6 rounded-lg shadow-md hover:bg-gray-700 transition duration-300 mt-[20px]"
+          className="ml-[370px] self-center justify-center bg-gray-800 text-white text-lg font-medium py-2 px-6 rounded-lg shadow-md hover:bg-gray-700 transition duration-300 mt-[20px]"
         >
           Saiba mais
         </Link>
