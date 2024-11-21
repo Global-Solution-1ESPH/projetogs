8827d706d553f973c1605448940a39c05a5cb78f
Gustavo Viega Martins Lopes <gvmlopes@gmail.com>
2024-11-21 01:18:26 -0300

    Adicionando a imagem no Sobre Nós

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -16,7 +16,7 @@ const Sobre =()=>{
         <div className="flex justify-around">
           
           <div className="bg-white max-w-sm rounded overflow-hidden shadow-xl">
-            <img className="w-full" src="/img/card-top.jpg" alt="Smart Energy"/>
+            <img className="w-full" src="../src/assets/solucao2.jpg" alt="Smart Energy"/>
 
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Smart Energy</div>
